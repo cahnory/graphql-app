21cad4a0956712e5a6b024e237db930cb80f1400
cahnory <cahnory@gmail.com>
2015-09-27 15:14:29 +0200

    Improve sass middleware options

--- src/http/sass.js
+++ src/http/sass.js
@@ -1,9 +1,29 @@
 import Middleware from 'node-sass-middleware';
 
-export default function (options) {
-    let middleware = Middleware(options);
-    return function *(next) {
-        yield middleware.bind(middleware, this.req, this.res);
-        yield next;
+const PROD_ENV = process.env.NODE_ENV === 'production';
+console.log(PROD_ENV);
+
+export default function (options = {}) {
+  // convert src to options object
+  if ('string' === typeof options) {
+    options = {
+      src: options
     };
+  }
+
+  // comporessed style in prod by default
+  if (!options.hasOwnProperty('outputStyle')) {
+    options.outputStyle = PROD_ENV ? 'compressed' : 'expanded';
+  }
+
+  // don't debug in prod by default
+  if (!options.hasOwnProperty('debug')) {
+    options.debug = !PROD_ENV;
+  }
+
+  let middleware = Middleware(options);
+  return function *(next) {
+      yield middleware.bind(middleware, this.req, this.res);
+      yield next;
+  };
 };
